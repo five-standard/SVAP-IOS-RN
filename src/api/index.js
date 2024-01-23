75bae8fb380064d7276fafba61ba8aed0b967488
six-standard <dbrrlwns1127@gmail.com>
2024-01-23 23:27:07 +0900

    error: warning 유발제 제거

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,6 @@
 import axios from "axios";
 import Asyncstorage from "@react-native-async-storage/async-storage";
 
-export * from "./Petition";
-export * from "./Report";
-export * from "./User";
-export * from "./Vote";
-
 export const instance = axios.create({
   baseURL: "https://prod-server.xquare.app/svap",
   timeout: 3000,
