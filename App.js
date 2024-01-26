c042987d5dab7f85429633e6fbcb5833d00a8813
six-standard <dbrrlwns1127@gmail.com>
2024-01-26 22:29:46 +0900

    chore: 유저 정보 불러오기 기능 추가

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
-import { getToken } from "./src/utils/strToken";
+import { delToken, getToken } from "./src/utils/strToken";
 import { StackNavigation } from "./src/navigators/Stack";
+import { getInfo } from "./src/api/User";
+import { queryKeys } from "./src/utils/queryKeys";
 
 const queryClient = new QueryClient();
 
@@ -14,6 +16,15 @@ export default function App() {
       const data = await getToken();
       if (data.accessToken !== null) {
         setAuth(true);
+        getInfo()
+          .then((res) => {
+            console.log(res.data);
+            queryClient.setQueryData(queryKeys.user, {
+              userName: res.data.userName,
+              accountId: res.data.accountId,
+            });
+          })
+          .catch(() => {});
       }
     }
     test();
