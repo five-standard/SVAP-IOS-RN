5bb1c0e045112453cdcb1d1747a1ff35ce10d484
six-standard <dbrrlwns1127@gmail.com>
2024-01-29 22:26:44 +0900

    chore: Input 필요한 코드 추가

--- src/components/common/Input.jsx
+++ src/components/common/Input.jsx
@@ -3,7 +3,7 @@ import { useRef, useState } from "react";
 import Ionicons from "@expo/vector-icons/Ionicons";
 
 export const Input = (props) => {
-  const { name, onChange, value, password, search, onSearch } = props;
+  const { name, onChange, value, style, password, search, onSearch } = props;
   const ref = useRef();
   const [focus, setFocus] = useState(false);
   const [visible, setVisible] = useState(false);
@@ -12,30 +12,34 @@ export const Input = (props) => {
 
   return (
     <TouchableOpacity
-      style={{
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
-        gap: 1,
-        borderColor: `${
-          search ? "transparent" : focus ? "#419FFF" : "#BEBEBE"
-        }`,
-        backgroundColor: `${search && "#F4F4F4"}`,
-        borderWidth: 1,
-        padding: 15,
-        borderRadius: 10,
-        width: "100%",
-      }}
+      style={[
+        {
+          flexDirection: "row",
+          justifyContent: "space-between",
+          gap: 1,
+          borderColor: `${
+            search ? "transparent" : focus ? "#419FFF" : "#BEBEBE"
+          }`,
+          backgroundColor: `${search && "#F4F4F4"}`,
+          borderWidth: 1,
+          padding: 15,
+          borderRadius: 10,
+          width: "100%",
+        },
+        style,
+      ]}
       activeOpacity={1}
       onPress={() => ref.current.focus()}
     >
       <TextInput
         {...props}
-        style={{ width: "90%" }}
+        style={{ width: "90%", height: "100%" }}
         onChangeText={(text) => onChange({ name, text })}
         onFocus={handleFocus}
         onBlur={handleFocus}
+        value={value}
         ref={ref}
+        textAlignVertical="top"
         secureTextEntry={password && !visible ? true : false}
       />
       {password && (
